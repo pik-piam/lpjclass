b82af9cf3e3feeb60c4d38eeb673754e34cfa016
mppalves <mppalves@gmail.com>
2020-05-26 12:07:55 +0200

    Adding a new argument to read.LPJ_input (ncells). Bug fixing integer overflow by converting in_nbytes into double

--- R/read.LPJ_input.R
+++ R/read.LPJ_input.R
@@ -6,7 +6,7 @@
 #' far, tested for landuse input.
 #' 
 #' @usage read.LPJ_input(file_name, out_years=c("y1995","y2005"),
-#' namesum=FALSE, four_d=FALSE,swap="little")
+#' namesum=FALSE, four_d=FALSE, ncells=59199, swap="little")
 #' @param file_name Filename with extension and folder
 #' @param out_years years to be red out in the form of a vector of year
 #' strings, e.g. c(y1995,y2005)
@@ -14,6 +14,7 @@
 #' useful to handle large datasets. Overwrites four_d
 #' @param four_d if true, it is assumed that data exists for both rainfed and
 #' irrigated crops.
+#' @param ncells number of cells 
 #' @param swap Depends on the binary format of the data
 #' @return \item{x}{LPJ-object}
 #' @author Benjamin Bodirsky, Susanne Rolinski
@@ -24,6 +25,7 @@ read.LPJ_input <- function(file_name,               # Filename with or without e
                     out_years=c("y1995","y2005"),                 # first year of simulation (standard=1901)
                     namesum=FALSE, 
                     four_d=FALSE,
+                    ncells=59199,
                     swap="little"
                     ) {
   #require(ludata)       
@@ -43,27 +45,30 @@ read.LPJ_input <- function(file_name,               # Filename with or without e
   #grid_67420_59199[which(grid_67420_59199>=1)]<-1
   #landusedata$grid_67420_59199<-grid_67420_59199
   
-    
-  out_ncells<-length(grid_67420_59199[which(grid_67420_59199!=0)])
+  if(ncells==67420){
+    out_ncells<-length(grid_67420_59199)
+  } else if(ncells==59199){
+    out_ncells<-length(grid_67420_59199[which(grid_67420_59199!=0)])    
+  } else {stop("Wrong number of cells provided (use 67420 or 59199).")}  
 
   filedata<-file(description = file_name, open = "rb", blocking = TRUE,encoding = getOption("encoding"))
     seek(filedata,where=7,origin="start")
-    in_header <- readBin(filedata,what=integer(),size=4,n=7,endian=.Platform$endian)
-    in_syear<-in_header[3]
-    in_nyears<-in_header[4]
-    in_ncells<-in_header[6]
-    in_ncolumns<-in_header[7]
+    in_header   <- as.numeric(readBin(filedata,what=integer(),size=4,n=7,endian=.Platform$endian))
+    in_syear    <- in_header[3]
+    in_nyears   <- in_header[4]
+    in_ncells   <- in_header[6]
+    in_ncolumns <- in_header[7]
     seek(filedata,where=7+7*4,origin="start")
-    in_header<-readBin(filedata,what=numeric(),size=4,n=2,endian=.Platform$endian)
-    in_cellsize<-in_header[1]
-    in_scalar<-in_header[2]
+    in_header   <- readBin(filedata,what=numeric(),size=4,n=2,endian=.Platform$endian)
+    in_cellsize <- in_header[1]
+    in_scalar   <- in_header[2]
 
-    in_years<-paste("y",in_syear+(1:in_nyears)-1,sep="")
-    in_nbytes<-2
+    in_years    <- paste("y",in_syear+(1:in_nyears)-1,sep="")
+    in_nbytes   <- 2
   
 
     in_file_size <- file.info(file_name)$size
-    if(in_file_size!=in_headbytes+in_nyears*in_ncells*in_ncolumns*in_nbytes){stop("file size does not fit header")}
+    if(in_file_size!=in_headbytes+in_nyears*in_ncells*in_ncolumns*as.double(in_nbytes)){stop("file size does not fit header")}
 
     read_year_set<-function(year) {
       if((year%in%in_years)==FALSE){stop("out_year is not in the dataset")}
@@ -71,7 +76,7 @@ read.LPJ_input <- function(file_name,               # Filename with or without e
       startread<-in_headbytes + (year_position-1)*in_ncells*in_ncolumns*in_nbytes
       seek(filedata,where=startread,origin="start")
       yeardataset <- array(readBin(filedata,what=integer(),size=in_nbytes,n=in_ncolumns*in_ncells,endian=swap),dim=c(in_ncolumns,in_ncells))
-      yeardataset<-yeardataset[,which(grid_67420_59199!=0)]
+      if(ncells==59199) yeardataset<-yeardataset[,which(grid_67420_59199!=0)]
       return(t(yeardataset))
     }
 
